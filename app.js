962f99a68d04e674b618a48da9f970154cdd630a
Agustin Macazzaga <ag.macazz@gmail.com>
2022-01-28 16:23:38 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const buttonOne = document.getElementById("1");
+const buttonTwo = document.getElementById("2");
+const buttonThree = document.getElementById("3");
+const buttonFour = document.getElementById("4");
+const buttonFive = document.getElementById("5");
+const buttonSix = document.getElementById("6");
+const buttonSeven = document.getElementById("7");
+const buttonEight = document.getElementById("8");
+const buttonNine = document.getElementById("9");
+const buttonClear = document.getElementById("clear");
+const buttonSum = document.getElementById("sum");
+const buttonSubstraction = document.getElementById("substraction");
+const buttonMultiplication = document.getElementById("multiplication");
+const buttonDivision = document.getElementById("division");
+const buttonResult = document.getElementById("result");
+const visor = document.getElementById("visor");
+//declarar .value de cada boton//
+buttonOne.value = buttonOne.id;
+console.log(buttonOne.value);
